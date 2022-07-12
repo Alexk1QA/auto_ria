65b9e934d3ff24b52d8f94b79a09914f6bac7e37
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-07-12 23:00:35 +0300

    first commit

--- route/route.py
+++
@@ -1,8 +0,0 @@
-
-"https://developers.ria.com/auto/categories/?api_key=0700hMiViMYsSFf6bhrG8nTK99LG9tKYEh4bwC1W"
-
-url = "developers.ria.com/"
-
-route_categories = "auto/categories/"
-
-route_bodystyles = "bodystyles"
